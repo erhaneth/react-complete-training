c3438ecd017cce608b6afaf9ef15bfde57bb5183
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-04-11 16:14:51 -0700

    add expense components

---
+++ src/components/Expenses.js
@@ -0,0 +1,21 @@
+// Expenses.js
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expenses = props.expenses;
+  return (
+    <div className="expenses">
+      {expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
