5437e735a0ed3506b24eeb49e690ae653603698b
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-04-12 17:48:41 -0700

    add card component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 // Expenses.js
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import Card from "./Card";
 
 function Expenses(props) {
   const expenses = props.expenses;
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
@@ -14,7 +15,7 @@ function Expenses(props) {
           date={expense.date}
         />
       ))}
-    </div>
+    </Card>
   );
 }
 
