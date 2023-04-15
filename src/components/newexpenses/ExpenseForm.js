5f9f7b7a854dc38f26ed24c110695a5800d059ea
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-04-15 08:08:59 -0700

    get user input

--- src/components/newexpenses/ExpenseForm.js
+++ src/components/newexpenses/ExpenseForm.js
@@ -1,21 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  // const [enteredTitle, setEnteredTitle] = useState("");
+  // const [enteredAmount, setEnteredAmount] = useState();
+  // const [enteredDate, setEnteredDate] = useState();
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+  const titleChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
+  };
+  const amountChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
+  };
+  const dateChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="1992-21-05" max="2023-21-05" />
+          <input
+            type="date"
+            min="1992-21-05"
+            max="2023-21-05"
+            onChange={setEnteredDate}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
