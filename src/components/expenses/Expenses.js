c9e4b178427993e600e32311985f481b3aa6bcc2
“erhaneth” <98863443+erhaneth@users.noreply.github.com>
2023-04-17 01:40:05 -0700

    add expense filter

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -2,20 +2,32 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "../UI/Card";
+import ExpenseFilter from "./ExpensesFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChageHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   const expenses = props.expenses;
   return (
-    <Card className="expenses">
-      {expenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChageHandler}
         />
-      ))}
-    </Card>
+        {expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
 };
 
